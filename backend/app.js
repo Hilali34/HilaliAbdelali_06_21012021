f49f10fc29d07642d311ab23be41788c24da1412
Hilali <Abdelali>
2021-07-25 16:20:31 +0200

    ajout authentifiaction et crud basique

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const saucesRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
+
+
+mongoose.connect("mongodb+srv://hilali34:hilali26130@cluster0.jvw3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 
 const app = express();
@@ -12,5 +24,10 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
