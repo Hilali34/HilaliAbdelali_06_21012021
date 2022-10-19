986502f8d9fc4426397d6e0ae55281c2c29c0696
hilali34 <hilali,abdelali@icloud.com>
2022-10-19 17:30:34 +0200

    make it better

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require('dotenv').config();
+require('dotenv').config({path:"./.env"});
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const path = require("path");
